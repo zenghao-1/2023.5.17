0fac1053ac38085760a94295f0bebaf715485d2f
林岚 <72348612+zenghao-1@users.noreply.github.com>
2023-05-17 21:40:57 +0800

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string Hell = "Hello,wordl!";
+	for (int i = 0; i < Hell.length(); i++)
+	{
+		cout << Hell[i] << " ";
+	}
+
+}
